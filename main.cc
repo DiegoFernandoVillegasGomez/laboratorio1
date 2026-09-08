a4b87f9d99fd305bf8ba5887511e7864f09f280f
Diego Villegas <villegasfernando021@gmail.com>
2026-09-08 14:23:21 -0600

    feat: estructura del codigo

---
+++ main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ 
+
+    int opcion;
+
+    cout << "Ingrese la opcion que quiere realizar" << endl;
+    cout << "1. Consultar saldo" << endl;
+    cout << "2. Depositar" << endl;
+    cout << "3. Retirar" << endl;
+    cout << "4. Salir" << endl;
+    cin >> opcion;
+
+    switch (opcion)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
