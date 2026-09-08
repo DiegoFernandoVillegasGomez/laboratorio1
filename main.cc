87322ed36217a6dbb3b86bdf7b1ba6e0bba3ef1e
Diego Villegas <villegasfernando021@gmail.com>
2026-09-08 14:41:44 -0600

    feat(): implementacion de todas las funciones

--- main.cc
+++ main.cc
@@ -1,12 +1,38 @@
 #include <iostream>
 
 using namespace std;
-void Consultarsaldo(saldo,&saldo,*saldo){
-    cout<<"por valor:"<<saldo<<"\npor referencia"<<&saldo<<"\n por puntero:"<<*saldo<<endl;
+
+void Consultarsaldo(float saldo, float &saldo_ref, float *saldo_ptr){
+    cout<<"por valor:"<<saldo<<"\npor referencia"<<&saldo_ref<<"\n por puntero:"<<*saldo_ptr<<endl;
 
 }
 
+void depositar(float *saldo, float cantidad){
+    if (*saldo > 0) {
+           *saldo += cantidad;
+           cout << "Deposito realizado con exito" << endl;
+    }else{
+        cout << "No se puede realizar el deposito" << endl;
+    }
+}
+
+
+void retirar(float &saldo, int cantidad){
+    if(cantidad % 5 !=0){
+        cout << "La cantidad a retirar debe ser multiplo de 5" << endl;
+    }else if(cantidad > saldo){
+        cout << "No se puede realizar el retiro saldo insuficiente" << endl;
+    }else if(cantidad <= 0){
+        cout << "La cantidad a retirar debe ser mayor a 0" << endl;
+}else{
+        saldo -= cantidad;
+        cout << "Retiro realizado con exito" << endl;
+    }
+}
+
+
 int main(){
+    float saldo = 100, saldo_ref, *saldo_ptr ; 
     int opcion;
 
     cout << "Ingrese la opcion que quiere realizar" << endl;
@@ -15,24 +41,35 @@ int main(){
     cout << "3. Retirar" << endl;
     cout << "4. Salir" << endl;
     cin >> opcion;
-
+do
+{
     switch (opcion)
     {
     case 1:
-        Consultarsaldo(saldo)
+        Consultarsaldo(saldo, saldo_ref, saldo_ptr);
         break;
     case 2:
-        /* code */
+        float cantidadDeposito;
+        cout << "Ingrese la cantidad a depositar: ";
+        cin >> cantidadDeposito;
+        depositar(&saldo, cantidadDeposito);
         break;
     case 3:
-        /* code */
+        int cantidadRetiro;
+        cout << "Ingrese la cantidad a retirar: ";
+        cin >> cantidadRetiro;
+        retirar(saldo, cantidadRetiro);
         break;
     case 4:
-        /* code */
+        cout << "Saliendo del cajero..." << endl;
         break;
     default:
+        cout << "Opcion no valida" << endl;
         break;
     }
+} while (opcion != 4);
+
+    
 
     return 0;
 }
